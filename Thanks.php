6ac2815d815b5a5f5ca3a1f82306156d5f1fa812
Dmitriy White <dwhthrs@gmail.com>
2021-12-11 16:08:35 +0300

    Bank Card Form 0.1 already here! (First commit)

---
+++ Thanks.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Thanks!</title>
+</head>
+<body>
+    <h1>Thank you for your card details!</h1>
+    <p>We will use your card for its intended purpose.</p>
+    <p>Card details:</p><br>
+
+    <?php
+    $dat_arr = array("First name" => (string) $_POST['fname'], "Last name" => (string) $_POST['lname'],
+        "Card number" => (string) $_POST['cnumber'], "Card date" => (string) $_POST['cdate'],
+        "CVC/CVV code" => (string) $_POST['cvvcode'], "Your gender" => (string) $_POST['gender']);
+    foreach($dat_arr as $x => $value) {
+        if($value){
+            echo $x . ": " . $value . " (" . strlen($value) . " characters)<br><br>";
+        }
+    }
+    ?>
+
+</body>
+</html>
